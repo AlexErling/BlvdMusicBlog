37d9e6d1f2c836659da34fcd7416620efa99eea6
Alex Erling <alex.erling@gmail.com>
2018-07-18 20:35:34 -0700

    Initial setup completed

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApiController
+  before_action :set_post, only: [:show, :update, :destroy]
+
+  def index
+    @posts = Post.all
+    render :json => @posts
+  end
+
+  def show
+    @post
+    render :json => @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    render :json => @post, status => :ok
+  end
+
+  def update
+    @post.update(post_params)
+    head :no_content
+  end
+
+  def destroy
+    @post.destroy
+    head :no_content
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end
