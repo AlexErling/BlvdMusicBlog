a9e32be06faf69655a17c660fa6f5d23bf9ac86d
Alex Erling <alex.erling@gmail.com>
2018-07-19 20:52:41 -0700

    Added validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,19 @@ class Post < ApplicationRecord
   has_one_attached :image
   enum post_type: [ :Song, :Playlist, :Video, :Event ]
   acts_as_taggable
-  # validate :correct_image_type
-  #
-  # def correct_image_type
-  #   if image.attached? && !image.content_type.in?(%w(image/jpeg image/png))
-  #       errors.add(:image, "Must be a JPEG or PNG.")
-  #   end
-  # end
+
+
+  #Validations
+  validate :correct_image_type
+  validates :name, :presence => true
+  validates :post_type, :presence => true
+  validates :title, length: { maximum: 150}, :presence => true
+  validates :body, :presence => true
+
+  def correct_image_type
+    if image.attached? && !image.content_type.in?(%w(image/jpeg image/png))
+        errors.add(:image, "Must be a JPEG or PNG.")
+    end
+  end
 
 end
